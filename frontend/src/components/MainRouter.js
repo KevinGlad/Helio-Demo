93a77b09d1fc19f0b5a93b6c1ab500f8790e647c
Kevin Glad <kglad@wbteam.net>
2020-09-14 09:05:03 -0600

    added example of passing props to protected route

--- frontend/src/components/MainRouter.js
+++ frontend/src/components/MainRouter.js
@@ -18,7 +18,9 @@ export default function MainRouter() {
                     <Route path="/signup" component={Signup}></Route>
                     <Route path="/login" component={Login}></Route>
                     <Route path="/forgotpass" component={ForgotPass}></Route>
-                    <ProtectedRoute path="/admin" component={EditUsers} test="test"></ProtectedRoute>
+                    <ProtectedRoute path="/admin" component={EditUsers}></ProtectedRoute>
+                    {/* https://ui.dev/react-router-v4-pass-props-to-components/ */}
+                    {/* <ProtectedRoute path="/admin" render={(props) => (<EditUsers {...props} test="tester"></EditUsers>)}></ProtectedRoute> */}
                     <Route path="/" component={Home}></Route>
                 </Switch>
             </Router>
